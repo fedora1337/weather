67d5c2ded34c4c9d34d6441529ec43b686e9ac5b
Fedora1337 <choccholanghe@gmail.com>
2026-05-10 19:19:34 +0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -68,7 +68,7 @@ def get_history(city):
 # -----------------------------
 st.title("🌤️ Ứng dụng dự đoán thời tiết Việt Nam")
 
-cities = ["Hà Nội", "Đà Nẵng", "Thành phố Hồ Chí Minh", "Huế", "Cần Thơ","Hải Phòng"," GJóng Nai"]
+cities = ["Hà Nội", "Đà Nẵng", "Thành phố Hồ Chí Minh", "Huế", "Cần Thơ","Hải Phòng","Tinh GJóng Nai"]
 city = st.selectbox("Chọn thành phố:", cities)
 
 if st.button("🔍 Xem thời tiết"):
