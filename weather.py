9d876b168ee1ef6661bb2f42f10a84ca8fe0831e
Fedora1337 <choccholanghe@gmail.com>
2026-05-17 19:37:36 +0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -67,7 +67,7 @@ def get_history(city):
 # Giao diện chính
 # -----------------------------
 st.title("🌤️ Ứng dụng dự đoán thời tiết Việt Nam")
-citytextinput = st.text_input("Nhập tên thành phố:", "gray[*_Ví dụ: Hà Nội_*]")
+citytextinput = st.text_input("Nhập tên thành phố:", "Ví dụ: Hà Nội")
 cities = ["Hà Nội", "Đà Nẵng", "Thành phố Hồ Chí Minh", "Huế", "Cần Thơ","Hải Phòng"]
 cityselectbox = st.selectbox("Thành phố đề xuất:", cities)
 
